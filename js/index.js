236a1d9cc23cce29d00854b4767d1697af431c44
Ayush <ayush.kumar20@yahoo.in>
2020-10-25 00:45:58 +0530

    update

--- js/index.js
+++ js/index.js
@@ -13,6 +13,24 @@ function funProfile(){
   document.getElementById("helpDiv").classList.add("noDisplay");
   document.getElementById("profileDiv").classList.toggle("noDisplay");
 }
-  
 
+window.onclick = function (event) {
+  if (event.target == document.getElementById("helpDiv") || event.target == document.getElementById("profileDiv")
+    || event.target == document.getElementById("modalDiv")) {
+    document.getElementById("helpDiv").classList.add("noDisplay");
+    document.getElementById("profileDiv").classList.add("noDisplay");
+    hideModal();
+  }
+}
 
+function hideModal(){
+  document.getElementById("modalDiv").classList.add("noDisplay");
+  document.getElementById("modalBtnDiv").classList.add("noDisplay");
+  document.getElementById("modalWarnDiv").classList.add("noDisplay");
+}
+
+function showModal(){
+  document.getElementById("modalDiv").classList.remove("noDisplay");
+  document.getElementById("modalBtnDiv").classList.remove("noDisplay");
+  document.getElementById("modalWarnDiv").classList.remove("noDisplay");
+}
